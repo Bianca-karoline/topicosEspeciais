2a3554f0beca08cb139c5dec65f9e62839c599b0
Bianca-karoline <113278939+Bianca-karoline@users.noreply.github.com>
2023-04-12 21:20:38 -0300

    Create chama_modulo.js

---
+++ aula06/chama_modulo.js
@@ -0,0 +1,6 @@
+let modulo = require('./arquivo_modulo.js'); //importação do arquivo
+
+console.log('Valor da variavel: ' + modulo.minhaVariavel);
+modulo.minhaFuncao();
+
+console.log('Valor da soma: ' + modulo.soma(15,20));
